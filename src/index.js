03891cf8e7af8ca5cb23f0d4a8fb83a4cc846c3e
Матвей Волков <flexbookmatvey@icloud.com>
2021-12-23 05:08:33 +0500

    Added User events

--- src/index.js
+++ src/index.js
@@ -1,48 +1,92 @@
-Vue.component('todo-list', {
-    props: ['placeholder', 'tasks', 'message'],
-    data: function(){
-        return {
-            tasksInput: "",
-            completed: false,
-            img: "/src/img/61569c9b6f025f1bae21bc242c27e321.jpg"
-        }
+Vue.component('like-button', {
+    model: {
+        prop: 'counter',
+        event: 'on-counter-change',
     },
+    props: ['counter'],
     template:
-        '<form onsubmit="return false;">' +
-            '<div v-if="!count()"> Ты выплонил все задачи!</div>' +
-            '<div v-else-if="count() == 1"> Почти получилось, продолжай в том же духе</div>' +
-            '<div v-else-if="count() == 2"> У тебя ещё есть над чем работать. Продолжай!</div>' +
-            '<div v-else>Осталось сделать задач: <span class="counter">{{ count() }}</span></div>'+
-            '<div class="list">\n' +
-            '        <div class="item" v-for="task in tasks">\n' +
-            '            <input type="checkbox" :disabled="task.disabled" v-model="task.completed">\n' +
-            '            <span :class="{done: task.completed}">{{ task.content}}\n</span>' +
-            '        </div>\n' +
-            '    </div>' +
-            '<div class="form">\n' +
-            '        <input :placeholder="placeholder" :title="message" v-model="this.tasksInput">\n' +
-            '        <button type="submit" v-on:click="this.addTask">Добавить</button>\n' +
-            '    </div>' +
-            '<transition name="fade"><img :src="this.img" alt="Robert Downey Jr congrats you" v-show="count() == 0"> </transition>' +
-        '</form>',
-    methods: {
-        addTask: function(){
-            this.tasks.push({content: tasksInput, disabled: false})
-        },
-        count: function (){
-            return this.tasks.filter(task => !task.completed).length;
+        "<span>" +
+            "<button @click='increase'>" +
+            "   &#9829; {{ counter }}" +
+            "</button>" +
+        "</span>",
+    methods:{
+        increase(){
+            this.$emit('on-counter-change', this.counter + 1)
         }
     }
+})
+Vue.component('dislike-button', {
+    model: {
+        prop: 'counter',
+        event: 'on-counter-change',
     },
-)
-
+    props: ['counter'],
+    template:
+        "<button @click='increase'>" +
+        "   ФУ {{counter}}" +
+        "</button>",
+    methods: {
+        increase: function (){
+            this.$emit('on-counter-change', this.counter + 1)
+        }
+    }
+})
+Vue.component('task-list', {
+    props: ['tasks', 'title'],
+    template: `
+    <div v-if="tasks.length != 0">
+        <div class="list">
+        <h2 v-if="title">{{ title }}:</h2>
+            <div class="item" :class="{done: task.completed}" v-for="(task, index) in tasks" :key="task.text + index">
+                <input type="checkbox" v-model="task.completed">
+                {{ task.content }}
+                <like-button v-model="task.likes"></like-button>
+                <dislike-button v-model="task.dislikes"></dislike-button>
+            </div>
+        </div>
+    </div>
+    `,
+})
 let vm = new Vue({
     el: '#app',
     data: {
+        headerDislikes: 0,
+        headerLikes: 0,
+        inputLikes: 0,
+        inputDislikes: 0,
         inputPlaceholder: 'Имя задания!',
-        message: 'Здесь должно быть имя задания, которое ты должен будешь выполнить',
-        tasks: [{content: 'Развернуть окружение в Codepen', disabled: false, completed: false},
-            {content: 'Сделать что-то', disabled: false, completed: false},
-            {content: 'Сделать интернет-магазин на Vue', disabled: false, completed: false}]
+        tasksInput: "",
+        tasks: [
+            {content: 'Развернуть окружение в Codepen', completed: false, likes: 0, dislikes: 0},
+            {content: 'Сделать что-то', completed: false, likes: 0, dislikes: 0},
+            {content: 'Сделать интернет-магазин на Vue', completed: false, likes: 0, dislikes: 0}],
+        img: "/src/img/61569c9b6f025f1bae21bc242c27e321.jpg"
+    },
+    methods: {
+        addTask: function(){
+            this.tasks.push({content: tasksInput, disabled: false, completed: false, likes: 0, dislikes: 0})
+        },
+    },
+    computed: {
+        getUncompletedTasksCount(){
+            console.log('getUncompletedTasksCount')
+            return this.tasks.filter(task => !task.completed).length;
+        },
+        uncompletedTasks(){
+            console.log('uncompletedTasks')
+            return this.tasks.filter(task => !task.completed)
+        },
+        completedTasks(){
+            console.log('completedTasks')
+            return this.tasks.filter(task => task.completed)
+        },
+        countLikes(){
+            return this.headerLikes + this.inputLikes + this.tasks.reduce((value, task) => value + task.likes, 0)
+        },
+        countDislikes(){
+            return this.headerDislikes + this.inputDislikes + this.tasks.reduce((value, task) => value + task.dislikes, 0)
+        }
     }
 })
+
